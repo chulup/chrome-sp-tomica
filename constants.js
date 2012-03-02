d5b3feccf21f8049e011962779b5b5a1b7d16bcc
Michael Shulbaev <chulupey@gmail.com>
2012-03-03 01:24:59 +0700

    Changed debug timeouts.

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 var icon_logged_in = "logged_in.png";
 var icon_logged_out = "logged_out.png";
 
-var pollInterval = 1000 * 60 /10;
+var pollInterval = 1000 * 60;
 var SP_CAL = 'http://sp.tomica.ru/forum/sp/mycalendar/mycalendar.php';
 var SP_BASE = 'http://sp.tomica.ru/forum/phpBB3/';
