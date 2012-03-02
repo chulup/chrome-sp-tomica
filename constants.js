3a706dfd799bb0657cd3f5e3b88d3ee7be64396f
Michael Shulbaev <chulupey@gmail.com>
2012-03-02 10:37:36 +0700

    Testing jQuery $.get

---
+++ constants.js
@@ -0,0 +1,6 @@
+var icon_logged_in = "logged_in.png";
+var icon_logged_out = "logged_out.png";
+
+var pollInterval = 1000 * 60 /10;
+var SP_CAL = 'http://sp.tomica.ru/forum/sp/mycalendar/mycalendar.php';
+var SP_BASE = 'http://sp.tomica.ru/forum/phpBB3/';
